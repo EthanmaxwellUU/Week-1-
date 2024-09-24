e4bf6f25f3ddfc8a778682938924d05e4f6da0c7
Csf23mae <Csf23mae@MF124PC18>
2024-09-24 15:56:15 +0100

    Add project files.

---
+++ Week 1 Pratical/Program.cs
@@ -0,0 +1,37 @@
+﻿//Task 1
+
+//Console.WriteLine("Please enter your name");
+//String name =Console.ReadLine();
+
+//Console.WriteLine("Please enter your age");
+//int age = Convert.ToInt32(Console.ReadLine());
+//int newage = Convert.ToInt32( age + 5);
+
+//Console.WriteLine("Hello " +name+" you will be, " +newage+" in 5 years");
+//Console.ReadKey();
+
+//Task 2
+
+//Console.WriteLine("How old are you?");
+//int agetwo = Convert.ToInt32(Console.ReadLine());
+
+
+//if (agetwo <= 0 || agetwo>=110) 
+//Console.WriteLine("Age out of range");
+
+//else if (agetwo <=13) 
+//Console.WriteLine("You are a child");
+
+//else if (agetwo >13 && agetwo <=19) 
+//Console.WriteLine("You are a teenager");
+
+//else if (agetwo > 19 && agetwo < 110) 
+//Console.WriteLine("You are an adult");
+
+//Task 3
+
+
+Console.WriteLine("Please enter your age");
+int age = Convert.ToInt32(Console.ReadLine());
+
+
